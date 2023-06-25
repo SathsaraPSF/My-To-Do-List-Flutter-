1aab83aa5337695ab83eb7b54f6112c2bac4b8d3
agro-pest <sathsara.psf99@gmail.com>
2023-06-25 17:31:25 +0530

    design settungs screen

--- lib/Screens/SettingsScreen.dart
+++ lib/Screens/SettingsScreen.dart
@@ -1,16 +1,65 @@
 import 'package:flutter/material.dart';
-
-import 'CardComponent.dart';
+import 'BackGroundImage.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: CardComponent(),
-      ),
+    return Stack(
+      children: [
+        const BackgroundImage(sourse: 'images/back_bg3.jpg'),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Padding(
+            padding: const EdgeInsets.only(top: 60, left: 20),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 150,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Image(
+                          image: AssetImage('images/user.png'),
+                          height: 60,
+                          width: 60),
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "Pasindu Sathsara",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'FontSubHeader',
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            "sat@gmail.com",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'FontSubHeader',
+                                color: Colors.white54,
+                                fontWeight: FontWeight.w100),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 60),
+                      IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.penToSquare,size: 20,color: Colors.white,)
+
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
