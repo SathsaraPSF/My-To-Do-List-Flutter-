37e8433a8d620d4c2bc602523d0da9b0630b0138
agro-pest <sathsara.psf99@gmail.com>
2023-06-28 16:34:08 +0530

    Create new file

---
+++ lib/Screens/WorkTodoScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class WorkTodo extends StatefulWidget {
+  const WorkTodo({super.key});
+
+  @override
+  State<WorkTodo> createState() => _WorkTodoState();
+}
+
+class _WorkTodoState extends State<WorkTodo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
